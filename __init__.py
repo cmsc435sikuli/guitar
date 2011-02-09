fdbf144722d73261b50f0e9dc40dc7dee4561ab0
jsternberg <jonathansternberg@gmail.com>
2011-02-09 15:42:09 -0500

    Modified to permit plugins to use custom JVM settings.

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@
 import os, subprocess, pickle
 from collections import namedtuple
 
-plugin = namedtuple('plugin', 'name doc classpath')
+plugin = namedtuple('plugin', 'name doc classpath sysproperty')
 
 class ui(object):
     def __init__(self, configPath = None):
@@ -22,10 +22,17 @@ class ui(object):
             with open(self._pathname, 'w') as f:
                 pickle.dump(self._cfg, f)
 
-    def install(self, name, doc, plugin_, classpath):
+    def install(self, name, doc, plugin_, classpath, sysproperty = None):
+        if sysproperty is None:
+            sysproperty = []
         print 'installing %s plugin' % name
-        self._cfg[name] = plugin(plugin_, doc, [os.path.abspath(path)
-                                                for path in classpath])
+        print [os.path.abspath(path) for path in classpath]
+        self._cfg[name] = \
+            plugin(plugin_, doc,
+                   [os.path.abspath(path)
+                    for path in classpath],
+                   [(n, os.path.abspath(v))
+                     for n, v in sysproperty])
 
     def uninstall(self, name):
         if self._cfg.has_key(name):
@@ -39,10 +46,12 @@ class ui(object):
         if not self._cfg.has_key(name):
             print 'plugin %s not available' % name
             return 1
-        plugin, doc, classpath = self._cfg[name]
+        plugin, doc, classpath, sysproperty = self._cfg[name]
 
         # Launch java with the appropriate classpath and main function
-        cmd = ['java', '-cp', ':'.join(classpath), main, plugin]
+        cmd = ['java']
+        cmd.extend(['-D%s=%s' % (name, value) for name, value in sysproperty])
+        cmd.extend(['-cp', ':'.join(classpath), main, plugin])
         cmd.extend(args)
         try:
             ret = subprocess.call(cmd)
